aef512ab58c5883e3d773f5d6239375e838dc8eb
Luis Eduardo Cantillo Zapateiro <cantilloluiseduardo52@gmail.com>
2024-11-26 18:28:09 -0500

    agregando url de backend

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://tu-dominio.com/api' // Cambia esta URL para producción
+  baseURL: process.env.NODE_ENV === 'production'
+    ? 'https://backend-music-one.vercel.app/' // Cambia esta URL para producción
     : 'http://localhost:3000/api',  // URL local para desarrollo
 });
 
